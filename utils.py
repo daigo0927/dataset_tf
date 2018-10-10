1f0e5a6dc13df0b72acde854bf1061d1bb14be98
Daigo HIROOKA <Daigo@Daigo-no-MacBook-Air.local>
2018-10-11 06:18:24 +0900

    add cropper

--- utils.py
+++ utils.py
@@ -1,5 +1,22 @@
+import random
 import tensorflow as tf
 
-class StaticCenterCrop(object):
-    def __init__(self, crop_size):
+class RandomCropper(object):
+    def __init__(self, image_size, crop_size):
         self.th, self.tw = crop_size
+        h, w = image_size
+        self.h1 = random.randint(0, h - self.th)
+        self.w1 = random.randint(0, w - self.tw)
+
+    def __call__(self, image):
+        return image[self.h1:(self.h1+self.th), self.w1:(self.w1+self.tw)]
+
+
+class CenterCropper(object):
+    def __init__(self, image_size, crop_size):
+        self.h, self.w = image_size
+        self.th, self.tw = crop_size
+
+    def __call__(self, image):
+        return image[(self.h-self.th)//2:(self.h+self.th)//2,
+                     (self.w-self.tw)//2:(self.w+self.tw)//2]
