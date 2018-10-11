21b1227d8c40e41553abdb8881a07076fcd88378
Daigo Hirooka <daigo@gorgon.murata.eb.waseda.ac.jp>
2018-10-11 17:58:05 +0900

    working confirmed

--- flow.py
+++ flow.py
@@ -1,13 +1,16 @@
+import random
 import numpy as np
 import tensorflow as tf
 from pathlib import Path
 from imageio import imread
 from functools import partial
+from itertools import groupby
 from abc import abstractmethod, ABCMeta
 
 import pdb
 
 import utils
+
 
 def load_flow(uri):
     """
@@ -25,61 +28,8 @@ def load_flow(uri):
         return None
 
 
-def _parse(filenames):
-# def _parse(image_0_path, image_1_path, flow_path):
-    """
-    Parser for optical flow datasets
-    Args:
-    - tuple<str> filenames: tuple containing image and flow paths (image_0, image_1, flow)
-    Returns:
-    - tf.Tensor images: paired continuous images
-    - tf.Tensor flow: ground truth optical flow between paired images
-    """
-    return tf.py_func(_read_py_func, [filenames], [tf.uint8, tf.uint8, tf.float32])
-
-def _read_py_func(filenames):
-    """ decode binary string to UTF-8 """
-    image_0_path, image_1_path, flow_path = filenames
-    image_0 = imread(image_0_path.decode())
-    image_1 = imread(image_1_path.decode())
-    flow = load_flow(flow_path.decode())
-    return image_0, image_1, flow
-
-
-def _preprocess(image_0, image_1, flow,
-                crop_type = 'random', crop_shape = None):
-    """
-    Preprocess the raw data
-    Args:
-    - tf.Tensor images: raw images
-    - tf.Tensor flow: raw flow
-    - str crop_type: crop type for raw images and flow ['random', 'center', None]
-    - tuple<int> crop_shape: shape of crop
-    Returns:
-    - tf.Tensor images: processed images
-    - tf.Tensor flow: processed flow
-    """
-
-    if crop_type is not None:
-        image_0 = tf.py_func(_crop_py_func, [image_0, crop_type, crop_shape], tf.uint8)
-        image_1 = tf.py_func(_crop_py_func, [image_1, crop_type, crop_shape], tf.uint8)
-        flow = tf.py_func(_crop_py_func, [flow, crop_type, crop_shape], tf.float32)
-
-    images = tf.stack([image_0, image_1], axis = 0)
-    images = tf.cast(images, tf.float32)
-    images = images/255.
-
-    return images, flow
-
-def _crop_py_func(x, crop_type, crop_shape):
-    cropper = utils.RandomCropper(x.shape[:2], crop_shape) if crop_type == 'random'\
-      else utils.CenterCropper(x.shape[:2], crop_shape)
-    x = cropper(x)
-    return x
-
-
 class BaseDataset(metaclass = ABCMeta):
-    """ Wrapper class of flexibly utilize tf.data pipeline """
+    """ Wrapper class to flexibly utilize tf.data pipeline """
     def __init__(self, dataset_dir, train_or_val,
                  crop_type = 'random', crop_shape = None,
                  shuffle = False, batch_size = 1, num_parallel_calls = 0):
@@ -93,15 +43,7 @@ class BaseDataset(metaclass = ABCMeta):
         if not train_or_val in ['train', 'val']:
             raise ValueError('train_or_val is either train or val')
         self.train_or_val = train_or_val
-        
-        if not crop_type in ['random', 'center', None]:
-            raise ValueError('crop_type should be in random/center/None')
-        if crop_type == 'random':
-            self.cropper = partial(utils.RandomCropper, crop_shape = crop_shape)
-        elif crop_type == 'center':
-            self.cropper = partial(utils.CenterCropper, crop_shape = crop_shape)
-        else:
-            self.cropper = None
+
         self.crop_type = crop_type
         self.crop_shape = crop_shape
 
@@ -127,6 +69,19 @@ class BaseDataset(metaclass = ABCMeta):
     def has_no_txt(self):
         pass
 
+    def split(self, samples):
+        p = Path(self.dataset_dir)
+        val_ratio = 0.1
+        random.shuffle(samples)
+        idx = int(len(samples) * (1 - val_ratio))
+        train_samples = samples[:idx]
+        val_samples = samples[idx:]
+
+        with open(p / 'train.txt', 'w') as f: f.writelines((','.join(i) + '\n' for i in train_samples))
+        with open(p / 'val.txt', 'w') as f: f.writelines((','.join(i) + '\n' for i in val_samples))
+
+        self.samples = train_samples if self.train_or_val == 'train' else val_samples
+
     def parse(self, filenames):
         """
         Tensorflow file parser using native python function
@@ -138,8 +93,8 @@ class BaseDataset(metaclass = ABCMeta):
         return tf.py_func(self._read_py, [filenames], [tf.uint8, tf.uint8, tf.float32])
 
     def _read_py(self, filenames):
-        """ python function for read image and flow data """
-        img_0_path, img_1_path, flow_path = filenames
+        """ Native python function for read image and flow data """
+        image_0_path, image_1_path, flow_path = filenames
         image_0 = imread(image_0_path.decode())
         image_1 = imread(image_1_path.decode())
         flow = self.load_flow(flow_path.decode())
@@ -151,10 +106,9 @@ class BaseDataset(metaclass = ABCMeta):
 
     def preprocess(self, image_0, image_1, flow):
         """ Function to preprocess raw images and optical flow """
-        if self.cropper is not None:
-            image_0 = tf.py_func(self.cropper, [image_0], tf.uint8)
-            image_1 = tf.py_func(self.cropper, [image_1], tf.uint8)
-            flow = tf.py_func(self.cropper, [flow], tf.float32)
+        if self.crop_shape is not None:
+            image_0, image_1, flow = tf.py_func(self._crop_py, [image_0, image_1, flow],
+                                                [tf.uint8, tf.uint8, tf.float32])
 
         images = tf.stack([image_0, image_1], axis = 0)
         images = tf.cast(images, tf.float32)
@@ -162,12 +116,24 @@ class BaseDataset(metaclass = ABCMeta):
 
         return images, flow
 
+    def _crop_py(self, image_0, image_1, flow):
+        """ Native python function for cropping """
+        image_size = image_0.shape[:2]
+        if self.crop_type == 'random':
+            cropper = utils.RandomCropper(image_size, self.crop_shape)
+        elif self.crop_type == 'center':
+            cropper = utils.CenterCropper(image_size, self.crop_shape)
+        else:
+            raise ValueError('invalid cropping argument has found')
+        image_0, image_1, flow = map(cropper, [image_0, image_1, flow])
+        return image_0, image_1, flow
+            
     def _build(self):
-        self.dataset = tf.data.Dataset.from_tensor_slices(self.samples)
+        self._dataset = tf.data.Dataset.from_tensor_slices(self.samples)
         if self.shuffle:
-            self.dataset = self.dataset.shuffle(len(self.samples))
+            self._dataset = self._dataset.shuffle(len(self.samples))
 
-        self.dataset = (self.dataset.map(self.parse, self.num_parallel_calls)
+        self._dataset = (self._dataset.map(self.parse, self.num_parallel_calls)
                         .map(self.preprocess, self.num_parallel_calls)
                         .batch(self.batch_size)
                         .repeat()
@@ -175,29 +141,54 @@ class BaseDataset(metaclass = ABCMeta):
         return
 
     def make_one_shot_iterator(self):
-        return self.dataset.make_one_shot_iterator()
+        return self._dataset.make_one_shot_iterator()
 
 
-if __name__ == '__main__':
-    import os
-    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-    sess = tf.Session()
+class FlyingChairs(BaseDataset):
+    def __init__(self, dataset_dir, train_or_val,
+                 crop_type = 'random', crop_shape = None,
+                 shuffle = False, batch_size = 1, num_parallel_calls = 0):
+        super().__init__(dataset_dir, train_or_val, crop_type, crop_shape,
+                         shuffle, batch_size, num_parallel_calls)
 
-    samples = []
-    with open('/Users/Daigo/Data/MPI-Sintel-complete/val.txt', 'r') as f:
-        for i  in f.readlines():
-            image_0_path, image_1_path, flow_path = i.split(',')
-            flow_path = flow_path.strip()
-            samples.append((image_0_path, image_1_path, flow_path))
+    def has_no_txt(self):
+        p = Path(self.dataset_dir) / 'data'
+        imgs = sorted(p.glob('*.ppm'))
+        samples = [(str(i[0]), str(i[1]), str(i[0]).replace('img1', 'flow').replace('.ppm', '.flo'))\
+                   for i in zip(imgs[::2], imgs[1::2])]
+        self.split(samples)
 
-    dataset = (tf.data.Dataset.from_tensor_slices(samples)
-               .shuffle(len(samples))
-               .map(_parse)
-               .map(partial(_preprocess, crop_type = 'random', crop_shape = (384, 448)))
-               .batch(4)
-               .prefetch(1))
 
-    iterator = dataset.make_one_shot_iterator()
-    next_el = iterator.get_next()
-    images, flow = sess.run(next_el)
-    print(f'image shape {images.shape}, flow shape {flow.shape}')
+class Sintel(BaseDataset):
+    def __init__(self, dataset_dir, train_or_val, mode = 'clean',
+                 crop_type = 'random', crop_shape = None,
+                 shuffle = False, batch_size = 1, num_parallel_calls = 0):
+        self.mode = mode
+        super().__init__(dataset_dir, train_or_val, crop_type, crop_shape,
+                         shuffle, batch_size, num_parallel_calls)
+
+    def has_no_txt(self):
+        p = Path(self.dataset_dir)
+        p_img = p / 'training' / self.mode
+        p_flow = p / 'training/flow'
+        
+        collections_of_scenes = sorted(map(str, p_img.glob('**/*.png')))
+        collections = [list(g) for k, g in groupby(collections_of_scenes, lambda x: x.split('/'[-2]))]
+        samples = [(*i, i[0].replace(self.mode, 'flow').replace('.png', '.flo'))\
+                    for collection in collections for i in utils.window(collection, 2)]
+        self.split(samples)
+
+
+class SintelClean(Sintel):
+    def __init__(self, dataset_dir, train_or_val, crop_type, crop_shape,
+                 shuffle, batch_size, num_parallel_calls):
+        super().__init__(dataset_dir, train_or_val, 'clean', crop_type, crop_shape,
+                         shuffle, batch_size, num_parallel_calls)
+
+class SintelFinal(Sintel):
+    def __init__(self, dataset_dir, train_or_val, crop_type, crop_shape,
+                 shuffle, batch_size, num_parallel_calls):
+        super().__init__(dataset_dir, train_or_val, 'final', crop_type, crop_shape,
+                         shuffle, batch_size, num_parallel_calls)
+
+
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import random
-import tensorflow as tf
+from itertools import islice
+
 
 class RandomCropper(object):
     def __init__(self, image_size, crop_size):
@@ -10,13 +11,23 @@ class RandomCropper(object):
 
     def __call__(self, image):
         return image[self.h1:(self.h1+self.th), self.w1:(self.w1+self.tw)]
-
+ 
 
 class CenterCropper(object):
-    def __init__(self, image_size, crop_size):
+    def __init__(self, image_sample, crop_size):
         self.h, self.w = image_size
         self.th, self.tw = crop_size
 
     def __call__(self, image):
         return image[(self.h-self.th)//2:(self.h+self.th)//2,
                      (self.w-self.tw)//2:(self.w+self.tw)//2]
+
+def window(seq, n = 2):
+    """ Returns a sliding window over data from the iterable """
+    it = iter(seq)
+    result = tuple(islice(it, n))
+    if len(result) == n:
+        yield result
+    for el in it:
+        result = result[1:] + (el,)
+        yield result
